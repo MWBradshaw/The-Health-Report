69071901e5759e781768a76101f5d228f9be3adf
MWBradshaw <matthewbradshaw875@gmail.com>
2025-05-06 20:52:17 -0500

    Added input elements for LoginForms and fixed rendering issue

--- src/components/Authentication/LoginForm.tsx
+++ src/components/Authentication/LoginForm.tsx
@@ -1,15 +1,17 @@
-import { FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
-import Box from '@mui/material/Box';
+import { Box, FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
 
 export const LoginForm = () => {
     return (
         <Box
             component="form"
           >
+
             <FormControl>
-              <InputLabel htmlFor="my-input">Email address</InputLabel>
-              <Input id="my-input" aria-describedby="my-helper-text" />
+              <InputLabel htmlFor="email-login">Email address</InputLabel>
+              <Input id="email-login" aria-describedby="my-helper-text" />
               <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+              <InputLabel htmlFor="password-login">Password</InputLabel>
+              <Input id="password-login" aria-describedby="my-helper-text" />
             </FormControl>
           </Box>
     )
