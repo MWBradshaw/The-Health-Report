790cf8c7cc88035fbcd334aec9c4260437564e38
MWBradshaw <matthewbradshaw875@gmail.com>
2025-05-07 20:35:35 -0500

    Add a stack for login form

--- src/components/Authentication/LoginForm.tsx
+++ src/components/Authentication/LoginForm.tsx
@@ -1,18 +1,21 @@
-import { Box, FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
+import { Box, FormControl, FormHelperText, Input, InputLabel, Stack } from '@mui/material';
 
 export const LoginForm = () => {
     return (
         <Box
             component="form"
           >
-
+            <Stack>
             <FormControl>
               <InputLabel htmlFor="email-login">Email address</InputLabel>
               <Input id="email-login" aria-describedby="my-helper-text" />
               <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+            </FormControl>
+            <FormControl>
               <InputLabel htmlFor="password-login">Password</InputLabel>
               <Input id="password-login" aria-describedby="my-helper-text" />
             </FormControl>
+            </Stack>
           </Box>
     )
 }
