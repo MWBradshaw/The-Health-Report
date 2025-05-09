722e5c1bf7b061419aec8862b869f53cb6ef5b20
MWBradshaw <matthewbradshaw875@gmail.com>
2025-05-08 20:35:21 -0500

    Changed the footer id

--- src/components/Authentication/LoginForm.tsx
+++ src/components/Authentication/LoginForm.tsx
@@ -9,7 +9,7 @@ export const LoginForm = () => {
             <FormControl>
               <InputLabel htmlFor="email-login">Email address</InputLabel>
               <Input id="email-login" aria-describedby="my-helper-text" />
-              <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+              <FormHelperText id="email-footer">We'll never share your email.</FormHelperText>
             </FormControl>
             <FormControl>
               <InputLabel htmlFor="password-login">Password</InputLabel>
